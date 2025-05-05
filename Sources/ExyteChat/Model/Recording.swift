5aa88a9dacf288724583c8ae4e9cc0bf798fcc08
Vlad Antonov <blackbird@live.ru>
2025-05-05 14:05:50 +0300

    Create Recording.swift

---
+++ Sources/ExyteChat/Model/Recording.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public struct Recording: Codable, Hashable, Sendable {
+    public var duration: Double
+    public var waveformSamples: [CGFloat]
+    public var url: URL?
+
+    public init(duration: Double = 0.0, waveformSamples: [CGFloat] = [], url: URL? = nil) {
+        self.duration = duration
+        self.waveformSamples = waveformSamples
+        self.url = url
+    }
+}
